b444f996a8f52659ca2a30006661cc9244c23f80
Davide Cacciarelli <83544651+dcacciarelli@users.noreply.github.com>
2023-05-22 14:16:36 +0200

    Update example.py

--- spc/autoencoders/oae/example.py
+++ spc/autoencoders/oae/example.py
@@ -1,1 +1,19 @@
+import pandas as pd
+from orthogonal_autoencoder.oae_pyspc.control_charts import ProcessMonitoring
 
+file_path = '/Users/dcac/Data/TEP_clean_33vars/'
+training_data = pd.read_csv(file_path+"train_fault_free_33columns.csv")
+training_data = training_data[training_data["simulationRun"] == 1].iloc[:, 3:]
+testing_data = pd.read_csv(file_path+"train_faulty_33columns.csv")
+testing_data = testing_data[(testing_data["simulationRun"] == 1) & (
+                testing_data["faultNumber"] == 1)].iloc[:, 3:]
+
+SPC = ProcessMonitoring(alpha=0.05)
+SPC.fit(data=training_data, encoding_layers=[33, 33], orthogonality_regularization=1, norm="l2",
+        nr_epochs=1000, patience=10, batch_size=10, verbose=True)
+SPC.plot_correlation_matrix()
+y_train_pred = SPC.plot_phase1(log_scale=True)
+y_test_pred = SPC.plot_phase2(data=testing_data, log_scale=True)
+out_of_control_obs = testing_data.iloc[161, :]
+SPC.t2_contribution_plots(ooc_observation=out_of_control_obs, method="monte_carlo", samples=1000)
+SPC.spe_contribution_plots(out_of_control_obs)
