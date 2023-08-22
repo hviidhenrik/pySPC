589826a75f65967ef241471e5894d97dc2491337
Henrik Hviid Hansen <HEHHA@orsted.dk>
2023-08-22 16:23:36 +0200

    added README.md, requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyspc",
+    version="1.0",
+    author="Henrik Hviid Hansen, Sebastian Olivier Nymann Topalian, Davide Cacciarelli",
+    author_email="hehha@orsted.com, sebtop@kt.dtu.dk, dcac@dtu.dk",
+    description="Plug and play statistical process control functions for Python",
+    long_description=long_description,
+    long_description_content_type="markdown",
+    url="https://github.com/hviidhenrik/pySPC",
+    packages=find_packages(exclude=["tests*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scikit_learn",
+        "scipy",
+        "statsmodels",
+        "torch"
+    ],
+)
